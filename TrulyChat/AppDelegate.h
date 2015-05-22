4b934751ab89b16900e618ecbfad210c1be2c221
Bruno Isola Reginato <brunoisola@ciandt.com>
2015-05-21 21:00:04 -0300

    Initial Commit

---
+++ TrulyChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrulyChat
+//
+//  Created by Bruno Isola Reginato on 5/21/15.
+//  Copyright (c) 2015 BReginato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrulyChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrulyChat
+//
+//  Created by Bruno Isola Reginato on 5/21/15.
+//  Copyright (c) 2015 BReginato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
